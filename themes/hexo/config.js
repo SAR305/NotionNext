01fe8ec11a0dc3149da451827ad1d15b925e97cd
SAR305 <72396900+SAR305@users.noreply.github.com>
2023-08-31 11:12:42 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -5,9 +5,9 @@ const CONFIG = {
 
   HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
-  HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
+  HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
   // 是否显示开始阅读按钮
-  SHOW_START_READING: false,
+  SHOW_START_READING: true,
 
   // 菜单配置
   MENU_INDEX: true, // 显示首页
@@ -31,7 +31,7 @@ const CONFIG = {
   WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   WIDGET_ANALYTICS: false, // 显示统计卡
   WIDGET_TO_TOP: true,
-  WIDGET_TO_COMMENT: false, // 跳到评论区
+  WIDGET_TO_COMMENT: true, // 跳到评论区
   WIDGET_DARK_MODE: true, // 夜间模式
   WIDGET_TOC: true // 移动端悬浮目录
 }
